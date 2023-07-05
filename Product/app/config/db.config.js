cef3166a82e01b1b25a6de222ca63fbfe06f854b
SauHanChan <schan253@my.centennialcollege.ca>
2023-07-05 13:47:37 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+//url:"mongodb+srv://Blessing:KF4CzZpj8tlYGtap@cluster0.29680fe.mongodb.net/SportStore?retryWrites=true&w=majority"
+url:"mongodb+srv://schan253:Hl7vRSXX0KLCdMLi@cluster0.ap2fuik.mongodb.net/DressStore?retryWrites=true&w=majority"
+
+};
